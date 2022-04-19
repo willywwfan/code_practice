44f930cc26490f1f95fdea4e351c4e7b111cd989
Willy-WW-Fan <Willy.WW.Fan@auo.com>
2022-04-19 14:39:03 +0800

    update test code

---
+++ C_test/binary_search.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int order_list[9] = {0, 1, 3, 4, 5, 6, 7, 8, 9};
+
+int binary_search(int data, int key){
+    int low = 0;
+    int upper = sizeof(order_list)/sizeof(order_list[0]) - 1
+    return 0;
+}
+
+int main(int argc, char** argv)
+{
+    printf("結果: %d \n", sizeof(order_list)/sizeof(order_list[0]));
+    return 0;
+}
---
+++ C_test/c_code/test.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<vector>
+#include<random>
+#include<ctime>
+#include<iterator>
+#include<algorithm>
+using namespace std;
